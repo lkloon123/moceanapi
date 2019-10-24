19ed25c70cfee99c19a4c29cf7524e0b74196b72
NeoSon <lkloon123@hotmail.com>
2019-10-24 15:49:20 +0800

    add moceanapi implementation

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -1,11 +1,28 @@
 <?php
 
-namespace NotificationChannels\:channel_namespace\Exceptions;
+namespace NotificationChannels\MoceanApi\Exceptions;
+
+use Mocean\Message\Message;
+use NotificationChannels\MoceanApi\MoceanApiSmsMessage;
 
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    /**
+     * @return static
+     */
+    public static function invalidReceiver()
     {
-        return new static("Descriptive error message.");
+        return new static('invalid receiver, please add routeNotificationForMoceanapi method to your notifiable');
+    }
+
+    /**
+     * @param mixed $message
+     * @return static
+     */
+    public static function invalidMessageObject($message)
+    {
+        $className = get_class($message) ?: 'Unknown';
+
+        return new static("Invalid message object `{$className}`, it should be `" . MoceanApiSmsMessage::class . '` or `' . Message::class . '`');
     }
 }
